11d93f76282edba813508b94b8788311a7ef002e
Clara-Ro <lara.rhee.gree@gmail.com>
2022-07-04 10:18:49 -0300

    Aplicativo de Jokepo pronto

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+
+import 'package:appteste/Jogo.dart';
+import 'package:flutter/material.dart';
+
+void main (){
+  runApp(MaterialApp(
+    home: Jogo(),
+    debugShowCheckedModeBanner: false,
+  ));
+}
